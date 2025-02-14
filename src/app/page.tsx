b3cb84763ee791d20b839b7e17a311ebdedc8b74
TangkaGoswamiPuja <web4dev2learning0@gmail.com>
2025-02-15 01:57:18 +0600

    just folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ export default function Home() {
   return (
 <div>
   hello next.js
+
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur laboriosam sed nisi quod reiciendis omnis inventore repudiandae tenetur cumque voluptate et nemo minima non adipisci aut ut, fugit dignissimos.</p>
 </div>    
   );
 }
